bbc4bc2729a3f4b2bd250b90db3595500f1a558c
Adom Z <zelalemadom@gmail.com>
2026-04-13 21:20:26 -0400

    feat: connect FCM payload to UI state updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import './screens/home_screen.dart';
 
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
